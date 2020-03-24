dfcda50ce0db1d6d4775cea9a1f665ce2f5500c5
g-otn <g_otn@hotmail.com>
2020-03-24 17:01:39 -0300

    add test code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, Platform, AlertController, ToastController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { CallNumber } from '@ionic-native/call-number/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,11 +16,15 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Platform,
+    CallNumber,
+    AlertController,
+    ToastController,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,8 @@
+import { CallNumber } from '@ionic-native/call-number/ngx';
+import { Platform, AlertController, ToastController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
 
 @Component({
   selector: 'app-folder',
@@ -8,11 +11,133 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder: string;
+  public imgsrc: string;
+  public result: string;
+  public err: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private platform: Platform,
+    private callNumber: CallNumber,
+    private alert: AlertController,
+    private toast: ToastController
+  ) { }
 
   ngOnInit() {
+    console.log(this.activatedRoute);
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+
+  capacitorGeolocation() {
+    Plugins.Geolocation.requestPermissions()
+      .then(value => {
+        this.result = this.json(value);
+        Plugins.Geolocation.getCurrentPosition({
+          enableHighAccuracy: true,
+          timeout: 3000
+        })
+          .then(value => {
+            this.result += this.json(value);
+          })
+      })
+      .catch(reason => this.err = this.json(reason));
+  }
+
+  capacitorSplashScreen() {
+    Plugins.SplashScreen.show({ autoHide: true });
+  }
+
+  capacitorPhotoPrompt() {
+    Plugins.Camera.requestPermissions()
+      .then(value => {
+        this.result = this.json(value);
+        Plugins.Camera.getPhoto({
+          source: CameraSource.Prompt,
+          resultType: CameraResultType.DataUrl,
+          saveToGallery: true,
+          quality: 10
+        })
+          .then(value => {
+            this.imgsrc = value.dataUrl
+          })
+      })
+      .catch(reason => this.err = this.json(reason));
+  }
+
+  capacitorPhotoGallery() {
+    Plugins.Camera.requestPermissions()
+      .then(value => {
+        this.result = this.json(value);
+        Plugins.Camera.getPhoto({
+          source: CameraSource.Photos,
+          resultType: CameraResultType.DataUrl,
+          saveToGallery: true,
+          quality: 10
+        })
+          .then(value => {
+            this.imgsrc = value.dataUrl
+          })
+      })
+      .catch(reason => this.err = this.json(reason));
+  }
+
+  capacitorPhotoCamera() {
+    Plugins.Camera.requestPermissions()
+      .then(value => {
+        this.result = this.json(value);
+        Plugins.Camera.getPhoto({
+          source: CameraSource.Camera,
+          resultType: CameraResultType.DataUrl,
+          saveToGallery: true,
+          quality: 10
+        })
+          .then(value => {
+            this.imgsrc = value.dataUrl
+          })
+      })
+      .catch(reason => this.err = this.json(reason));
+  }
+
+  capacitorToast() {
+    Plugins.Toast.show({
+      text: 'Teste toast 3s center',
+      duration: 'long',
+      position: 'center'
+    })
+  }
+
+  capacitorAlert() {
+    Plugins.PushNotifications.requestPermissions()
+      .then(value => {
+        this.result = this.json(value);
+      });
+  }
+
+  cordovaCallNumber(bypass = true) {
+    this.callNumber.callNumber('+5511900000000', bypass);
+  }
+
+  angularToast() {
+    this.toast.create({
+      header: 'toast angular',
+      message: 'mensagem',
+      buttons: ['Ok', 'Ok2']
+    }).then(value => {
+      value.present();
+    });
+  }
+
+  angularAlert() {
+    this.alert.create({
+      header: 'alert angular',
+      subHeader: 'sub header',
+      message: 'message',
+      buttons: ['Button 1', 'Button 2']
+    }).then(value => value.present());
+  }
+
+  json(value) {
+    return JSON.stringify(value, null, ' ');
   }
 
 }
